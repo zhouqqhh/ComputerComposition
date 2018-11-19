dcbf9d4dc2e5ffad5b879183cfd25193d0fd81f2
TedZhouJR <34849806+TedZhouJR@users.noreply.github.com>
2018-11-19 22:18:17 +0800

    完成 Mem to WB 锁存器

--- pipeline/EXEtoMEM.vhd
+++ pipeline/EXEtoMEM.vhd
@@ -4,6 +4,8 @@ use WORK.UTILS.ALL;
 
 entity EXEtoMEM is
 	port (
+		clk: in std_logic;
+		
 		in_control_Mem: in control_M_type;
 		in_control_WB: in control_WB_type;
 		in_ALUResult: in std_logic_vector(15 downto 0);
@@ -21,8 +23,29 @@ entity EXEtoMEM is
 end EXEtoMEM;
 
 architecture Behavioral of EXEtoMEM is
+signal EXEtoMEM_control_Mem: control_M_type;
+signal EXEtoMEM_control_WB: control_WB_type;
+signal EXEtoMEM_ALUResult: std_logic_vector(15 downto 0);
+signal EXEtoMEM_mem_ReadData: std_logic_vector(15 downto 0);
+signal EXEtoMEM_write_RegData: std_logic_vector(15 downto 0);
+signal EXEtoMEM_write_Register: std_logic_vector(2 downto 0);
 begin
-
+	process(clk)
+	begin
+		out_control_Mem <= EXEtoMEM_control_MEM;
+		out_control_WB <= EXEtoMEM_control_WB;
+		out_ALUResult <= EXEtoMEM_ALUResult;
+		out_mem_ReadData <= EXEtoMEM_mem_ReadData;
+		out_write_RegData <= EXEtoMEM_write_RegData;
+		out_write_Register <= EXEtoMEM_write_Register;
+		
+		EXEtoMEM_control_MEM <= in_control_Mem;
+		EXEtoMEM_control_WB <= in_control_WB;
+		EXEtoMEM_ALUResult <= in_ALUResult;
+		EXEtoMEM_mem_ReadData <= in_mem_ReadData;
+		EXEtoMEM_write_RegData <= in_write_RegData;
+		EXEtoMEM_write_Register <= in_write_Register;
+	end process;
 
 end Behavioral;
 
