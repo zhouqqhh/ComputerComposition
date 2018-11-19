5fb9a5d7958ea2480ab4de2fa7d1754b34f87f5b
TedZhouJR <34849806+TedZhouJR@users.noreply.github.com>
2018-11-19 17:02:39 +0800

    Create add.vhd

---
+++ pipeline/add.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity add is
+end add;
+
+architecture Behavioral of add is
+	port (
+		src1: in std_logic_vector(15 downto 0);
+		src2: in std_logic_vector(15 downto 0);
+		output: out std_logic_vector(15 downto 0);
+	);
+begin
+	output <= src1 + src2;
+end Behavioral;
+
